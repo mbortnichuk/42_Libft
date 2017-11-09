2a36f3a29828b9a528dbd297c9437966b7dd95e4
Mariana BORTNICHUK <mbortnic@e2r11p15.unit.ua>
2017-11-09 18:48:33 +0200

    changes to strsplit

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -1,79 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*   ft_strsplit2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbortnic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/06 19:00:47 by mbortnic          #+#    #+#             */
-/*   Updated: 2017/11/06 20:10:47 by mbortnic         ###   ########.fr       */
+/*   Created: 2017/11/08 16:23:34 by mbortnic          #+#    #+#             */
+/*   Updated: 2017/11/09 17:45:31 by mbortnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+ 
 #include <libft.h>
-
-static int	wordcounter(char const *s, char c)
-{
-	int c;
-	int w;
-	int one;
-
-	i = 0;
-	w = 0;
-	one = 1;
-	while (s[i])
-	{
-		if (s[i] != c && one)
-		{
-			w++;
-			one = 0;
-		}
-		else if (s[i] == c)
-		{
-			one = 1;
-		}
-		i++;
-	}
-	return (w);
-}
-
-static int	wordlength(const char *s, char c, int *start)
-{
-	int length;
-
-	while (s[*start] == c)
-	{
-		(*start)++;
-	}
-	length = 0;
-	while (s[*start + length] && s[*start + length] != c)
-	{
-		length++;
-	}
-	return (length);
-}
 
 char	**ft_strsplit(char const *s, char c)
 {
+	int i; /*counter **string arr*/
+	int j; /*counter *s arr*/
+	int w; /*counter for characters int **string*/
 	char **string;
-	int i;
-	int w;
-	int length;
 
-	if (!s)
+	if (!s || !(string = (char**)malloc(sizeof(*string) * ft_wordcount(s, c) + 1)))
 		return (NULL);
-	string = (char **)malloc(sizeof(char *) * wordcounter(s, c) + 1);
-	if (!string)
-		return (NULL);
-	w = 0;
+	j = 0;
 	i = 0;
-	while (w < wordcount(s, c))
+	while (i < ft_wordcount(s, c))
 	{
-		length = wordlength(s, c, &i);
-		string[w] = ft_strsub(s, i, length);
-		i += length;
-		w++;
+		w = 0;
+		if (!(string[i] = ft_strnew(ft_wordlength(&s[j], c) + 1)))
+			return (NULL);
+		while (s[j] == c)
+			j++;
+		while (s[j] != c && s[j])
+			string[i][w++] = s[j++];
+		string[i][w] = '\0';
+		i++;
 	}
-	str[w] = NULL;
+	string[i] = 0;
 	return (string);
 }
